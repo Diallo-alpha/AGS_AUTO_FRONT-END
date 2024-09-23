a65b2c44e96f037fb75bb2a416597e7d8ee870e5
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-23 23:55:37 +0000

    cnfiguration routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 export const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: ''},
   {path: '', component:AccueilComponent},
-  {path: 'inscription', component:InscriptionComponent}
+  {path: 'inscription', component:InscriptionComponent},
+  {path: 'login', component:ConnexionComponent},
 
 ];
