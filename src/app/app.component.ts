b97e6190c9693d45190df06b5c3a85c171056418
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-23 23:30:48 +0000

    integration de la  page de connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AccueilComponent, InscriptionComponent],
+  imports: [RouterOutlet, AccueilComponent, InscriptionComponent, ConnexionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
