ce272f93f1e5476a79486efcf4f0734e37ed26a0
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-23 20:24:57 +0000

    page inscription integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: ''},
+  {path: '', component:AccueilComponent},
+  {path: 'inscription', component:InscriptionComponent}
+
+];
