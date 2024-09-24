bb5d5ee50961892411f4ab88f8437a5db2c8263e
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-24 19:10:13 +0000

    navbar quand utilisateur est connecter

---
+++ src/app/nav-connect/nav-connect.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-connect',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-connect.component.html',
+  styleUrl: './nav-connect.component.css'
+})
+export class NavConnectComponent {
+
+}
