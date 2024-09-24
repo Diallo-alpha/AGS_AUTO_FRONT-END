c258fa4ee350b90d1d3d33f28bbc097ffd6681bf
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-24 15:51:33 +0000

    ajouter la page detail

---
+++ src/app/detailformation/detailformation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailformation',
+  standalone: true,
+  imports: [],
+  templateUrl: './detailformation.component.html',
+  styleUrl: './detailformation.component.css'
+})
+export class DetailformationComponent {
+
+}
