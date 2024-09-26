c437a48336796bda3d29d2b8943a860321366bba
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-26 12:07:27 +0000

    authenficiation des utilisateurs

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,26 +1,50 @@
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/authservice.service';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [RouterLink, FormsModule, CommonModule],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './connexion.component.html',
-  styleUrl: './connexion.component.css'
+  styleUrls: ['./connexion.component.css']
 })
-export class ConnexionComponent {
-  email: string = '';
-  password: string = '';
+export class ConnexionComponent implements OnInit {
+  loginForm: FormGroup = this.fb.group({});
+  errorMessage: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
   onSubmit() {
-    this.authService.login(this.email, this.password).subscribe({
-      next: () => this.router.navigate(['']),
-      error: (error) => console.error('Erreur de connexion', error)
-    });
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          console.log('Connexion réussie');
+          this.router.navigate(['']);
+        },
+        error: (error) => {
+          console.error('Erreur de connexion', error);
+          this.errorMessage = 'Email ou mot de passe incorrect.';
+        }
+      });
+    } else {
+      this.errorMessage = 'Veuillez remplir correctement tous les champs.';
+    }
   }
 }
