018b9fbc009c29b04149d1fa24d739797d0925d5
Diallo-alpha <alphaloppecity@gmail.com>
2024-09-30 18:57:41 +0000

    dynamique page accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -2,14 +2,67 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { ArticleService } from '../services/articleservice.service';
+import { ServiceService } from '../services/service.service';
+import { FormationService } from '../services/formation.service';
+import { PartenaireService } from '../services/partenaire.service';
+import { Formation } from '../models/FormationModel';
+import { Article } from '../models/ArticleModel';
+import { service } from '../models/serviceModel';
+import { partenaire } from '../models/partenaireModel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, FooterComponent],
+  imports: [RouterModule, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
+  formations: Formation[] = [];
+  services: service[] = [];
+  articles: Article[] = [];
+  partenaires: partenaire[] = [];
 
+  constructor(
+    private formationService: FormationService,
+    private serviceService: ServiceService,
+    private articleService: ArticleService,
+    private partenaireService: PartenaireService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadFormations();
+    this.loadServices();
+    this.loadArticles();
+    this.loadPartenaires();
+  }
+
+  //les formation
+  loadFormations() {
+    this.formationService.getAllFormations().subscribe((data: Formation[]) => {
+      this.formations = data.slice(0, 4); //4 premiere
+    });
+  }
+
+  //les services
+  loadServices() {
+    this.serviceService.getServices().subscribe((data: service[]) => {
+      this.services = data.slice(0, 4);
+    });
+  }
+
+  // les articles
+  loadArticles() {
+    this.articleService.getArticles().subscribe((data: Article[]) => {
+      this.articles = data.slice(0, 4);
+    });
+  }
+  //les partenaires
+  loadPartenaires() {
+    this.partenaireService.getPartenaires().subscribe((data: partenaire[]) => {
+      this.partenaires = data.slice(0, 4);
+    });
+  }
 }
