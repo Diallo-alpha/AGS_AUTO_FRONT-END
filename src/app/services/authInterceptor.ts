69a83059168e27251b69e8998bc887029e70f440
Diallo-alpha <alphaloppecity@gmail.com>
2024-10-01 13:40:31 +0000

    utiliser lurl deployer et ajouter le intercepteur pour gerer les cors

--- src/app/services/authInterceptor.ts
+++ src/app/services/authInterceptor.ts
@@ -2,13 +2,24 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access_token');
+
+  let clonedReq = req.clone({
+    setHeaders: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    withCredentials: true
+  });
+
   if (token) {
-    const cloned = req.clone({
+    clonedReq = clonedReq.clone({
       setHeaders: {
+        ...clonedReq.headers,
         Authorization: `Bearer ${token}`
       }
     });
-    return next(cloned);
   }
-  return next(req);
+
+  return next(clonedReq);
 };
