07825030e595198aee8ab81cb5f70e17e7a0e920
Diallo-alpha <alphaloppecity@gmail.com>
2024-10-01 15:40:53 +0000

    authintercepteur corriger

--- src/app/services/authInterceptor.ts
+++ src/app/services/authInterceptor.ts
@@ -3,12 +3,12 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access_token');
   if (token) {
-    const cloned = req.clone({
+    const clonedRequest = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(cloned);
+    return next(clonedRequest);
   }
   return next(req);
 };
