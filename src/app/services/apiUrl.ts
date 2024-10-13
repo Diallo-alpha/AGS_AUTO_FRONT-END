553d194c45e921c3f478e053b8eda5ca32f18137
Diallo-alpha <alphaloppecity@gmail.com>
2024-10-13 00:55:49 +0000

    changer url api

--- src/app/services/apiUrl.ts
+++ src/app/services/apiUrl.ts
@@ -1,4 +1,4 @@
-// export const apiUrl = "https://certif.alphaloppecity.simplonfabriques.com/api";
+export const apiUrl = "https://certif.alphaloppecity.simplonfabriques.com/api";
 /*api local*/
-export const apiUrl = "http://127.0.0.1:8000/api";
+// export const apiUrl = "http://127.0.0.1:8000/api";
 
